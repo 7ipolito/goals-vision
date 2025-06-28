55f917cd66ce42e0ecaa3c2897ced6fde0fd23bc
7ipolito <allanhipolito66@gmail.com>
2025-06-28 12:39:57 +0200

    chore: to solve problems

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["cdn.futbin.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.futbin.com",
+        pathname: "/content/fifa21/img/players/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
